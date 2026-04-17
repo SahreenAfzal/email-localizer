2722b1be86c54a1fb8309a5a9f00bad1bbed6824
SahreenAfzal <107059389+SahreenAfzal@users.noreply.github.com>
2026-04-17 17:48:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
 document.addEventListener("DOMContentLoaded", () => {
   const btn = document.getElementById("generateBtn");
 
-  console.log("button found:", btn);
-
-  btn.addEventListener("click", () => {
-    console.log("BUTTON CLICK WORKING");
-  });
+  btn.addEventListener("click", generate);
 });
+
+async function generate() {
+  console.log("generate function called");
+
+  const htmlFile = document.getElementById("htmlFile").files[0];
+
+  if (!htmlFile) {
+    alert("Upload HTML file");
+    return;
+  }
+
+  const html = await htmlFile.text();
+
+  try {
+    const res = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ html })
+    });
+
+    const data = await res.json();
+
+    document.getElementById("output").textContent = data.output;
+
+  } catch (e) {
+    alert("API error");
+    console.error(e);
+  }
+}
